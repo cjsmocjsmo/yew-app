c8e90b3cc0bcec4991cc703b6249e75a2e7368ae
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-01 12:59:13 -0700

    starwars added

--- src/images.rs
+++ src/images.rs
@@ -1,16 +1,23 @@
 
+pub mod andor_img_comp;
+pub mod bad_batch_img_comp;
+pub mod book_of_boba_fett_img_comp;
 pub mod discovery_img_comp;
 pub mod enterprise_img_comp;
 pub mod house_of_the_dragon_img_comp;
 pub mod lowerdecks_img_comp;
+pub mod mandalorian_img_comp;
 pub mod nextgeneration_img_comp;
 pub mod nineteen23_img_comp;
+pub mod obi_wan_kenobi_img_comp;
 pub mod picard_img_comp;
 pub mod prehistoric_planet_img_comp;
 pub mod prodigy_img_comp;
 pub mod strange_new_worlds_img_comp;
 pub mod sttv_img_comp;
+pub mod tales_of_the_jedi_img_comp;
 pub mod the_lord_of_the_rings_img_comp;
+pub mod visions_img_comp;
 pub mod voyager_img_comp;
 pub mod wheel_of_time_img_comp;
 pub mod y_the_last_man_img_comp;
--- src/tvshows/starwarspage.rs
+++ src/tvshows/starwarspage.rs
@@ -1,40 +1,103 @@
-use yew_router::prelude::*;
-use yew::prelude::*;
 use stylist::yew::use_style;
+use yew::prelude::*;
+// use yew_router::prelude::*;
 
 #[function_component]
 pub fn ImageGroupB() -> Html {
-    let img_div = use_style!("
+    let img_div = use_style!(
+        "
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
         justify-content: center;
         align-items: center;
         margin-bottom: 60px;
-    ");
-    let img_style = use_style!("
-        display: block;
-        margin-top: 10px;
-        margin-right: auto;
-        margin-left: auto;
-        width: 200px;
+    "
+    );
+
+    let img_container = use_style!("
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 210px;
+        height: 400px;
+        background-color: black;
+        border: 3px solid black;
         border-radius: 8px;
     ");
-    
-    let img_src = String::from(
-        "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22486-main.jpg"
-    );
+
+    let season_style = use_style!("
+        color:blue;
+        font-size: 1.75em;
+        text-decoration: underline;
+    ");
+
+    let season_div = use_style!("
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        width:100%;
+    ");
 
     html!(
         <>
             <div class={ img_div }>
+                <div class={img_container.clone()}>
+                    <crate::images::andor_img_comp::AndorImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
 
-                <img class={img_style.clone()} src={img_src.clone()} alt="video thumbnail" />
+                <div class={img_container.clone()}>
+                    <crate::images::bad_batch_img_comp::TheBadBatchImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                        <p class={season_style.clone()}>{"2"}</p>
+                    </div>
+                </div>
 
-                <img class={img_style.clone()} src={img_src.clone()} alt="video thumbnail" />
+                <div class={img_container.clone()}>
+                    <crate::images::book_of_boba_fett_img_comp::BookOfBobaFettImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
 
-                <img class={img_style.clone()} src={img_src.clone()} alt="video thumbnail" />
+                <div class={img_container.clone()}>
+                    <crate::images::mandalorian_img_comp::MandalorianImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                        <p class={season_style.clone()}>{"2"}</p>
+                        <p class={season_style.clone()}>{"3"}</p>
+                    </div>
+                </div>
 
+                <div class={img_container.clone()}>
+                    <crate::images::obi_wan_kenobi_img_comp::ObiWanKenobiImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                        <p class={season_style.clone()}>{"2"}</p>
+                    </div>
+                </div>
+
+                <div class={img_container.clone()}>
+                    <crate::images::visions_img_comp::VisionsImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>   
+                </div>
+
+                <div class={img_container.clone()}>
+                    <crate::images::tales_of_the_jedi_img_comp::TalesOfTheJediImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                        <p class={season_style.clone()}>{"2"}</p>
+                    </div>
+                </div>
             </div>
         </>
     )
@@ -42,13 +105,33 @@ pub fn ImageGroupB() -> Html {
 
 #[function_component]
 pub fn TVStarWarsPage() -> Html {
-    let navigator = use_navigator().unwrap();
+    let h1_style = use_style!(
+        "
+        text-align: center;
+        color: #ebb917;
+    "
+    );
 
-    let onclick = Callback::from(move |_| navigator.push(&crate::Route::Home));
+    let btn_group = use_style!(
+        "
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    "
+    );
+
     html!(
         <>
-            <h1 style="text-align:center;color:white">{"Star Wars Page"}</h1>
-            <button {onclick} style="padding:8px;">{"Home"}</button>
+            <div class={btn_group}>
+                <crate::comps::mainpage_comps::MusicSVG />
+                <crate::comps::mainpage_comps::MovieSVG />
+                <crate::comps::mainpage_comps::TVShowsSVG />
+            </div>
+            <h1 class={h1_style}>{"Star Wars"}</h1>
             <ImageGroupB />
 
         </>
