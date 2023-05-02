a94828bb6720836e4d6ce51f76d7984afb27ede2
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-02 01:52:19 -0700

    added more images

--- src/tvshows/mcupage.rs
+++ src/tvshows/mcupage.rs
@@ -1,4 +1,3 @@
-use yew_router::prelude::*;
 use yew::prelude::*;
 use stylist::yew::use_style;
 
@@ -12,29 +11,86 @@ pub fn ImageGroupB() -> Html {
         align-items: center;
         margin-bottom: 60px;
     ");
-    // let img_style = use_style!("
-    //     display: block;
-    //     margin-top: 10px;
-    //     margin-right: auto;
-    //     margin-left: auto;
-    //     width: 200px;
-    //     border-radius: 8px;
-    // ");
-    
-    // let img_src = String::from(
-    //     "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22486-main.jpg"
-    // );
+
+    let img_container = use_style!("
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 210px;
+        height: 400px;
+        background-color: black;
+        border: 3px solid black;
+        border-radius: 8px;
+    ");
+
+    let season_style = use_style!("
+        color:blue;
+        font-size: 1.75em;
+        text-decoration: underline;
+    ");
+
+    let season_div = use_style!("
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        width:100%;
+    ");
 
     html!(
         <>
             <div class={ img_div }>
 
-                <crate::images::prehistoric_planet_img_comp::PrehistoricPlanetImgComp />
-
-                <crate::images::nineteen23_img_comp::Nineteen23ImgComp />
-
-                <crate::images::y_the_last_man_img_comp::YTheLastManImgComp />
-
+                <div class={img_container.clone()}>
+                    <crate::images::falcon_and_the_winter_soldier_img_comp::FalconAndTheWinterSoldierImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::hawkeye_img_comp::HawkeyeImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::i_am_groot_img_comp::IAmGrootImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::loki_img_comp::LokiImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::moonknight_img_comp::MoonKnightImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::ms_marvel_img_comp::MsMarvelImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::she_hulk_img_comp::SheHulkImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
+                <div class={img_container.clone()}>
+                    <crate::images::wandavision_img_comp::WandaVisionImgComp />
+                    <div class={season_div.clone()}>
+                        <p class={season_style.clone()}>{"1"}</p>
+                    </div>
+                </div>
             </div>
         </>
     )
@@ -42,15 +98,32 @@ pub fn ImageGroupB() -> Html {
 
 #[function_component]
 pub fn MCUPage() -> Html {
-    let navigator = use_navigator().unwrap();
+    let btn_group = use_style!(
+        "
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    "
+    );
 
-    let onclick = Callback::from(move |_| navigator.push(&crate::Route::Home));
+    let h1_style = use_style!("
+        text-align: center;
+        color: #ebb917;
+    ");
+
     html!(
         <>
-            <h1 style="text-align:center;color:white">{"MCU Page"}</h1>
-            <button {onclick} style="padding:8px;">{"Home"}</button>
+            <div class={btn_group}>
+                <crate::comps::mainpage_comps::MusicSVG />
+                <crate::comps::mainpage_comps::MovieSVG />
+                <crate::comps::mainpage_comps::TVShowsSVG />
+            </div>
+            <h1 class={h1_style}>{"MCU"}</h1>
             <ImageGroupB />
-            <crate::images::prehistoric_planet_img_comp::PrehistoricPlanetImgComp />
         </>
     )
 }
